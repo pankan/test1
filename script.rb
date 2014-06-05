92da3637965b46feb3b8be99ef7bc5f81590c771
Pankan <kiran@qburst.com>
2014-06-05 17:39:15 +0530

    second

--- script.rb
+++ script.rb
@@ -2,12 +2,18 @@
 
 require 'rexml/document'
 require 'open-uri'
+require 'mysql'
+
 include REXML
 
+client = Mysql.new("localhost", "root", "pichathi", "parses")
+client.query("CREATE TABLE url( uid varchar(3), loc varchar(100))")
+i=0
 uri = URI.parse("http://www.qburst.com/sitemap.xml")
 uri.open {|f|
 	xmldoc = Document.new(f)
 	xmldoc.elements.each("urlset/url/loc"){ 
-		|e| puts "Location : " + e.text
+		 |e| client.query("INSERT INTO url (uid,loc) VALUES ('#{ i }','#{ e.text }')")
+          i=i+1
 	}
 }
