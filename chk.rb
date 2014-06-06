8b684a4befabda3e9ff01eeadf9a55a8d0fb5958
Kiran Surendran Pankan <pankan@users.noreply.github.com>
2014-06-06 17:45:27 +0530

    Delete chk.rb

--- chk.rb
+++
@@ -1,8 +0,0 @@
-#!/usr/local/bin/ruby -w 
-# create.rb
-require "mysql"
-# client = Mysql::Client.new(:host => "localhost", :username => "root", :password => "pichathi", :database => "sampledb")
-client = Mysql.new("localhost", "root", "pichathi", "sampledb")
-# client.select_db("sampledb")
-# client.query("CREATE TABLE url3( uid varchar(3), loc varchar(100))")
-client.query("INSERT INTO url2 VALUES('00A', 'http://www.qburst.com/')")
