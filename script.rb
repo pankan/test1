b5cbcddfde1cdbc3a705ab29ab4c11ab97aee312
Pankan <kiran@qburst.com>
2014-06-06 17:47:00 +0530

    Third

--- script.rb
+++ script.rb
@@ -3,17 +3,90 @@
 require 'rexml/document'
 require 'open-uri'
 require 'mysql'
+require 'pg'
+# require 'mongo'
 
 include REXML
 
-client = Mysql.new("localhost", "root", "pichathi", "parses")
-client.query("CREATE TABLE url( uid varchar(3), loc varchar(100))")
-i=0
-uri = URI.parse("http://www.qburst.com/sitemap.xml")
-uri.open {|f|
-	xmldoc = Document.new(f)
-	xmldoc.elements.each("urlset/url/loc"){ 
-		 |e| client.query("INSERT INTO url (uid,loc) VALUES ('#{ i }','#{ e.text }')")
-          i=i+1
+# MySQL
+
+class MySQLDirect
+	# 
+	def connect
+		@client = Mysql.new("localhost", "root", "pichathi", "parses")
+	end
+	def createurlTable
+		@client.query("CREATE TABLE urls( uid varchar(3), loc varchar(255))")
+	end
+
+	def addurl(i,e)
+		@client.query("INSERT INTO urls (uid,loc) VALUES ('#{ i }','#{ e.text }')")
+	end
+end
+
+
+# PostgreSQL
+
+class PostgresDirect
+  # Create the connection instance.
+  def connect
+    @conn = PG.connect(
+        :dbname => 'psampledb',
+        :user => 'intern500'
+    )
+  end
+  # Create table
+  def createurlTable
+    @conn.exec("CREATE TABLE urls (uid serial NOT NULL, loc character varying(255), CONSTRAINT urls_pkey PRIMARY KEY (uid)) WITH (OIDS=FALSE);");
+  end
+
+  # Prepared statements prevent SQL injection attacks.  However, for the connection, the prepared statements
+  # live and apparently cannot be removed, at least not very easily.  There is apparently a significant
+  # performance improvement using prepared statements.
+  def prepareInserturlStatement
+    @conn.prepare("insert_url", "insert into urls (uid, loc) values ($1, $2)")
+  end
+
+  # Add a url with the prepared statement.
+  def addurl(uid, loc)
+    @conn.exec_prepared("insert_url", [uid, loc])
+  end
+end
+
+# Main function
+def main
+
+	i = 1 # ID token
+
+	m = MySQLDirect.new
+	m.connect
+	m.createurlTable
+
+	p = PostgresDirect.new()
+  	p.connect
+  	p.createurlTable
+    p.prepareInserturlStatement
+
+    # Parsing XML
+	uri = URI.parse("http://www.qburst.com/sitemap.xml")
+
+	# Saving to DBs
+	uri.open {|f|
+		xmldoc = Document.new(f)
+		xmldoc.elements.each("urlset/url/loc"){ 
+			|e|
+
+			# Insert into MySQL DB
+			m.addurl(i,e)
+		
+			# Insert into Postgres DB
+			p.addurl(i, "#{ e.text }")
+		
+			# Insert into MongoDB
+			#
+
+			i=i+1 # Update ID token
+		}
 	}
-}
+end
+main
